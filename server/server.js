d2c5d2a0b7e18cb4fee9c853851a1ae4d5c61eb1
mirsahib <mirsahib24@gmail.com>
2021-09-03 13:00:52 +0600

    :hammer: Created a exportable server object

--- server/server.js
+++ server/server.js
@@ -9,9 +9,11 @@ mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
 
-app.listen(config.port, (err) => {
+const server =  app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
   }
   console.info('Server started on port %s.', config.port)
 })
+
+export default {server,mongoose}
